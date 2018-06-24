b7465445666a23f15310874784133beb39092dee
Roman Dawydkin <tanmatra@gmail.com>
2018-06-24 19:02:17 +0300

    Translate to Kotlin: WindingRule

--- src/main/java/java2d/demos/Paths/WindingRule.kt
+++ src/main/java/java2d/demos/Paths/WindingRule.kt
@@ -29,64 +29,60 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package java2d.demos.Paths;
+package java2d.demos.Paths
 
-
-import static java.awt.Color.BLACK;
-import static java.awt.Color.LIGHT_GRAY;
-import static java.awt.Color.WHITE;
-import java.awt.Graphics2D;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Path2D;
-import java2d.Surface;
-
+import java2d.Surface
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.geom.GeneralPath
+import java.awt.geom.Path2D
 
 /**
  * Rectangles filled to illustrate the GenerPath winding rule, determining
  * the interior of a path.
  */
-@SuppressWarnings("serial")
-public class WindingRule extends Surface {
-
-    public WindingRule() {
-        setBackground(WHITE);
+class WindingRule : Surface()
+{
+    init {
+        background = Color.WHITE
     }
 
-    @Override
-    public void render(int w, int h, Graphics2D g2) {
+    override fun render(w: Int, h: Int, g2: Graphics2D) {
+        g2.translate(w * 0.2, h * 0.2)
 
-        g2.translate(w * .2, h * .2);
+        val p = GeneralPath(Path2D.WIND_NON_ZERO)
+        p.moveTo(0.0f, 0.0f)
+        p.lineTo(w * 0.5f, 0.0f)
+        p.lineTo(w * 0.5f, h * 0.2f)
+        p.lineTo(0.0f, h * 0.2f)
+        p.closePath()
 
-        GeneralPath p = new GeneralPath(Path2D.WIND_NON_ZERO);
-        p.moveTo(0.0f, 0.0f);
-        p.lineTo(w * .5f, 0.0f);
-        p.lineTo(w * .5f, h * .2f);
-        p.lineTo(0.0f, h * .2f);
-        p.closePath();
+        p.moveTo(w * 0.05f, h * 0.05f)
+        p.lineTo(w * 0.55f, h * 0.05f)
+        p.lineTo(w * 0.55f, h * 0.25f)
+        p.lineTo(w * 0.05f, h * 0.25f)
+        p.closePath()
 
-        p.moveTo(w * .05f, h * .05f);
-        p.lineTo(w * .55f, h * .05f);
-        p.lineTo(w * .55f, h * .25f);
-        p.lineTo(w * .05f, h * .25f);
-        p.closePath();
+        g2.color = Color.LIGHT_GRAY
+        g2.fill(p)
+        g2.color = Color.BLACK
+        g2.draw(p)
+        g2.drawString("NON_ZERO rule", 0, -5)
 
-        g2.setColor(LIGHT_GRAY);
-        g2.fill(p);
-        g2.setColor(BLACK);
-        g2.draw(p);
-        g2.drawString("NON_ZERO rule", 0, -5);
+        g2.translate(0.0, h * 0.45)
 
-        g2.translate(0.0f, h * .45);
-
-        p.setWindingRule(Path2D.WIND_EVEN_ODD);
-        g2.setColor(LIGHT_GRAY);
-        g2.fill(p);
-        g2.setColor(BLACK);
-        g2.draw(p);
-        g2.drawString("EVEN_ODD rule", 0, -5);
+        p.windingRule = Path2D.WIND_EVEN_ODD
+        g2.color = Color.LIGHT_GRAY
+        g2.fill(p)
+        g2.color = Color.BLACK
+        g2.draw(p)
+        g2.drawString("EVEN_ODD rule", 0, -5)
     }
 
-    public static void main(String s[]) {
-        createDemoFrame(new WindingRule());
+    companion object {
+        @JvmStatic
+        fun main(s: Array<String>) {
+            createDemoFrame(WindingRule())
+        }
     }
 }
