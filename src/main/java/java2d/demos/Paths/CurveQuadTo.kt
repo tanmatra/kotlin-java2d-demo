38b2ac9026b132d4b581fa1e64e071d7d5efbbdd
Roman Dawydkin <tanmatra@gmail.com>
2018-06-24 19:08:27 +0300

    Translate to Kotlin: CurveQuadTo

--- src/main/java/java2d/demos/Paths/CurveQuadTo.kt
+++ src/main/java/java2d/demos/Paths/CurveQuadTo.kt
@@ -29,44 +29,41 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package java2d.demos.Paths;
+package java2d.demos.Paths
 
-
-import static java.awt.Color.BLACK;
-import static java.awt.Color.LIGHT_GRAY;
-import static java.awt.Color.WHITE;
-import java.awt.Graphics2D;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Path2D;
-import java2d.Surface;
-
+import java2d.Surface
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.geom.GeneralPath
+import java.awt.geom.Path2D
 
 /**
  * Cubic & Quad curves implemented through GeneralPath.
  */
-@SuppressWarnings("serial")
-public class CurveQuadTo extends Surface {
-
-    public CurveQuadTo() {
-        setBackground(WHITE);
+class CurveQuadTo : Surface()
+{
+    init {
+        background = Color.WHITE
     }
 
-    @Override
-    public void render(int w, int h, Graphics2D g2) {
-        GeneralPath p = new GeneralPath(Path2D.WIND_EVEN_ODD);
-        p.moveTo(w * .2f, h * .25f);
-        p.curveTo(w * .4f, h * .5f, w * .6f, 0.0f, w * .8f, h * .25f);
-        p.moveTo(w * .2f, h * .6f);
-        p.quadTo(w * .5f, h * 1.0f, w * .8f, h * .6f);
-        g2.setColor(LIGHT_GRAY);
-        g2.fill(p);
-        g2.setColor(BLACK);
-        g2.draw(p);
-        g2.drawString("curveTo", (int) (w * .2), (int) (h * .25f) - 5);
-        g2.drawString("quadTo", (int) (w * .2), (int) (h * .6f) - 5);
+    override fun render(w: Int, h: Int, g2: Graphics2D) {
+        val p = GeneralPath(Path2D.WIND_EVEN_ODD)
+        p.moveTo(w * 0.2f, h * 0.25f)
+        p.curveTo(w * 0.4f, h * 0.5f, w * 0.6f, 0.0f, w * 0.8f, h * 0.25f)
+        p.moveTo(w * 0.2f, h * 0.6f)
+        p.quadTo(w * 0.5f, h * 1.0f, w * 0.8f, h * 0.6f)
+        g2.color = Color.LIGHT_GRAY
+        g2.fill(p)
+        g2.color = Color.BLACK
+        g2.draw(p)
+        g2.drawString("curveTo", (w * 0.2).toInt(), (h * 0.25f).toInt() - 5)
+        g2.drawString("quadTo", (w * 0.2).toInt(), (h * 0.6f).toInt() - 5)
     }
 
-    public static void main(String s[]) {
-        createDemoFrame(new CurveQuadTo());
+    companion object {
+        @JvmStatic
+        fun main(s: Array<String>) {
+            createDemoFrame(CurveQuadTo())
+        }
     }
 }
