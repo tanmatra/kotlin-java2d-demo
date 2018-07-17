ab54292a23623e317586a66aaf3286a5786c9972
Roman Dawydkin <tanmatra@gmail.com>
2018-07-17 18:48:03 +0300

    Refactor: ColorConvert

--- src/main/java/java2d/demos/Colors/ColorConvert.kt
+++ src/main/java/java2d/demos/Colors/ColorConvert.kt
@@ -32,16 +32,8 @@
 package java2d.demos.Colors
 
 import java2d.Surface
+import java2d.use
 import java.awt.Color
-import java.awt.Color.black
-import java.awt.Color.blue
-import java.awt.Color.cyan
-import java.awt.Color.green
-import java.awt.Color.magenta
-import java.awt.Color.orange
-import java.awt.Color.pink
-import java.awt.Color.red
-import java.awt.Color.yellow
 import java.awt.Font
 import java.awt.Graphics2D
 import java.awt.Image
@@ -63,48 +55,43 @@ class ColorConvert : Surface()
     }
 
     override fun render(w: Int, h: Int, g2: Graphics2D) {
-        val iw = image.getWidth(this)
-        val ih = image.getHeight(this)
-        val frc = g2.fontRenderContext
-        val font = g2.font
-        g2.color = black
-        var tl = TextLayout("ColorConvertOp RGB->GRAY", font, frc)
-        tl.draw(g2,
-                (w / 2 - tl.bounds.width / 2).toFloat(),
-                tl.ascent + tl.leading)
+        val imageWidth = image.getWidth(this)
+        val imageHeight = image.getHeight(this)
+        g2.color = Color.BLACK
+        val titleTextLayout = TextLayout(TITLE, g2.font, g2.fontRenderContext)
+        titleTextLayout.draw(g2,
+                             (w / 2 - titleTextLayout.bounds.width / 2).toFloat(),
+                             titleTextLayout.ascent + titleTextLayout.leading)
 
-        val srcImg = BufferedImage(iw, ih, BufferedImage.TYPE_INT_RGB)
-        val srcG = srcImg.createGraphics()
-        val rhs = g2.renderingHints
-        srcG.setRenderingHints(rhs)
-        srcG.drawImage(image, 0, 0, null)
+        val hints = g2.renderingHints
+        val srcImg = BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB)
+        srcImg.createGraphics().use { srcGr ->
+            srcGr.setRenderingHints(hints)
+            srcGr.drawImage(image, 0, 0, null)
 
-        val s = "JavaColor"
-        val f = Font("serif", Font.BOLD, iw / 6)
-        tl = TextLayout(s, f, frc)
-        val tlb = tl.bounds
-        val chars = s.toCharArray()
-        var charWidth = 0.0f
-        val rw = iw / chars.size
-        val rh = ih / chars.size
-        for (i in chars.indices) {
-            tl = TextLayout(chars[i].toString(), f, frc)
-            val shape = tl.getOutline(null)
-            srcG.color = colors[i % colors.size]
-            tl.draw(srcG,
-                    (iw / 2 - tlb.width / 2 + charWidth).toFloat(),
-                    (ih / 2 + tlb.height / 2).toFloat())
-            charWidth += shape.bounds.getWidth().toFloat()
-            srcG.fillRect(i * rw, ih - rh, rw, rh)
-            srcG.color = colors[colors.size - 1 - i % colors.size]
-            srcG.fillRect(i * rw, 0, rw, rh)
+            val textFont = Font(Font.SERIF, Font.BOLD, imageWidth / 6)
+            val textLayout = TextLayout(TEXT, textFont, g2.fontRenderContext)
+            val textBounds = textLayout.bounds
+            var charX = 0.0f
+            val boxWidth = imageWidth / TEXT.length
+            val boxHeight = imageHeight / TEXT.length
+            for ((i, char) in TEXT.withIndex()) {
+                val singleCharLayout = TextLayout(char.toString(), textFont, g2.fontRenderContext)
+                val shape = singleCharLayout.getOutline(null)
+                srcGr.color = COLORS[i % COLORS.size]
+                singleCharLayout.draw(srcGr,
+                                      (imageWidth / 2 - textBounds.width / 2 + charX).toFloat(),
+                                      (imageHeight / 2 + textBounds.height / 2).toFloat())
+                charX += shape.bounds.getWidth().toFloat()
+                srcGr.fillRect(i * boxWidth, imageHeight - boxHeight, boxWidth, boxHeight)
+                srcGr.color = COLORS[COLORS.size - 1 - i % COLORS.size]
+                srcGr.fillRect(i * boxWidth, 0, boxWidth, boxHeight)
+            }
         }
 
-        val cs = ColorSpace.getInstance(ColorSpace.CS_GRAY)
-        val theOp = ColorConvertOp(cs, rhs)
-
-        val dstImg = BufferedImage(iw, ih, BufferedImage.TYPE_INT_RGB)
-        theOp.filter(srcImg, dstImg)
+        val operation = ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), hints)
+        val dstImg = BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB)
+        operation.filter(srcImg, dstImg)
 
         g2.drawImage(srcImg, 10, 20, w / 2 - 20, h - 30, null)
         g2.drawImage(dstImg, w / 2 + 10, 20, w / 2 - 20, h - 30, null)
@@ -112,7 +99,11 @@ class ColorConvert : Surface()
 
     companion object
     {
-        private val colors = arrayOf(red, pink, orange, yellow, green, magenta, cyan, blue)
+        private const val TITLE = "ColorConvertOp RGB->GRAY"
+        private const val TEXT = "JavaColor"
+
+        private val COLORS = arrayOf(Color.RED, Color.PINK, Color.ORANGE, Color.YELLOW, Color.GREEN,
+                                     Color.MAGENTA, Color.CYAN, Color.BLUE)
 
         @JvmStatic
         fun main(s: Array<String>) {
