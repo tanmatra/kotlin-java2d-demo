2cc56f8dc6c7d76aab5531ac9826e948a72adf7b
Roman Dawydkin <tanmatra@gmail.com>
2018-08-17 00:06:28 +0300

    Translate to Kotlin: Caps

--- src/main/java/java2d/demos/Lines/Caps.kt
+++ src/main/java/java2d/demos/Lines/Caps.kt
@@ -29,53 +29,45 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package java2d.demos.Lines;
+package java2d.demos.Lines
 
-
-import static java.awt.BasicStroke.CAP_BUTT;
-import static java.awt.BasicStroke.CAP_ROUND;
-import static java.awt.BasicStroke.CAP_SQUARE;
-import static java.awt.BasicStroke.JOIN_MITER;
-import static java.awt.Color.BLACK;
-import static java.awt.Color.WHITE;
-import java.awt.BasicStroke;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
-import java.awt.geom.Line2D;
-import java2d.Surface;
-
+import java2d.Surface
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.font.TextLayout
+import java.awt.geom.Line2D
 
 /**
  * Shows the three different styles of stroke ending.
  */
-@SuppressWarnings("serial")
-public class Caps extends Surface {
-
-    private static int cap[] = { CAP_BUTT, CAP_ROUND, CAP_SQUARE };
-    private static String desc[] = { "Butt Cap", "Round Cap", "Square Cap" };
-
-    public Caps() {
-        setBackground(WHITE);
+class Caps : Surface()
+{
+    init {
+        background = Color.WHITE
     }
 
-    @Override
-    public void render(int w, int h, Graphics2D g2) {
-        FontRenderContext frc = g2.getFontRenderContext();
-        Font font = g2.getFont();
-        g2.setColor(BLACK);
-        for (int i = 0; i < cap.length; i++) {
-            g2.setStroke(new BasicStroke(15, cap[i], JOIN_MITER));
-            g2.draw(new Line2D.Float(w / 4, (i + 1) * h / 4, w - w / 4, (i + 1)
-                    * h / 4));
-            TextLayout tl = new TextLayout(desc[i], font, frc);
-            tl.draw(g2, (float) (w / 2 - tl.getBounds().getWidth() / 2), (i + 1)
-                    * h / 4 - 10);
+    override fun render(w: Int, h: Int, g2: Graphics2D) {
+        val fontRenderContext = g2.fontRenderContext
+        val font = g2.font
+        g2.color = Color.BLACK
+        for ((i, cap) in CAPS.withIndex()) {
+            g2.stroke = BasicStroke(15f, cap, BasicStroke.JOIN_MITER)
+            val y = (i + 1) * h / (CAPS.size + 1).toFloat()
+            g2.draw(Line2D.Float(w / 4f, y, w - w / 4f, y))
+            val tl = TextLayout(DESCRIPTIONS[i], font, fontRenderContext)
+            tl.draw(g2, (w / 2 - tl.bounds.width / 2).toFloat(), y - 10)
         }
     }
 
-    public static void main(String s[]) {
-        createDemoFrame(new Caps());
+    companion object
+    {
+        private val CAPS = intArrayOf(BasicStroke.CAP_BUTT, BasicStroke.CAP_ROUND, BasicStroke.CAP_SQUARE)
+        private val DESCRIPTIONS = arrayOf("Butt Cap", "Round Cap", "Square Cap")
+
+        @JvmStatic
+        fun main(s: Array<String>) {
+            createDemoFrame(Caps())
+        }
     }
 }
