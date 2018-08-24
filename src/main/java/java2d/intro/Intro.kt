b56e931da08b7ddab9b60062a3edfdfa14141dad
Roman Dawydkin <tanmatra@gmail.com>
2018-08-24 16:20:38 +0300

    Move Part to separate file

--- src/main/java/java2d/intro/Intro.kt
+++ src/main/java/java2d/intro/Intro.kt
@@ -257,23 +257,6 @@ class Intro : JPanel(BorderLayout())
         }
 
         /**
-         * Part is a piece of the scene.  Classes must implement Part
-         * in order to participate in a scene.
-         */
-        internal interface Part
-        {
-            val begin: Int
-
-            val end: Int
-
-            fun reset(newWidth: Int, newHeight: Int)
-
-            fun step(w: Int, h: Int)
-
-            fun render(w: Int, h: Int, g2: Graphics2D)
-        }
-
-        /**
          * Scene is the manager of the parts.
          */
         internal class Scene(
