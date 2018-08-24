8a7f34c1d3ad78951e7e404e6cb10b9656eb3eff
Roman Dawydkin <tanmatra@gmail.com>
2018-08-24 20:24:45 +0300

    Refactor: SiE

--- src/main/java/java2d/intro/SiE.kt
+++ src/main/java/java2d/intro/SiE.kt
@@ -14,84 +14,74 @@ import java.util.ArrayList
  * image then rotate and scale down the subimages.
  */
 internal class SiE(
-    private val siw: Int,
-    private val sih: Int,
+    private val subimageWidth: Int,
+    private val subimageHeight: Int,
     override val begin: Int,
     override val end: Int
 ) : Part
 {
-    private var bimg: BufferedImage? = null
-    private val rIncr: Double = 360.0 / (end - begin)
-    private val sIncr: Double = 1.0 / (end - begin)
+    private var bufferedImage: BufferedImage? = null
+    private val rotateIncr: Double = 360.0 / (end - begin)
+    private val scaleIncr: Double = 1.0 / (end - begin)
     private var scale: Double = 0.0
     private var rotate: Double = 0.0
-    private val subs = ArrayList<BufferedImage>(20)
-    private val pts = ArrayList<Point>(20)
+    private val subimages = ArrayList<BufferedImage>(20)
+    private val points = ArrayList<Point>(20)
 
     override fun reset(newWidth: Int, newHeight: Int) {
         scale = 1.0
         rotate = 0.0
-        bimg = null
-        subs.clear()
-        pts.clear()
+        bufferedImage = null
+        subimages.clear()
+        points.clear()
     }
 
     override fun step(w: Int, h: Int) {
-        if (bimg == null) {
-            bimg = Intro.Surface.bufferedImage!!.createSimilar().also { bimg ->
-                bimg.createGraphics().use { big ->
+        if (bufferedImage == null) {
+            bufferedImage = Intro.Surface.bufferedImage!!.createSimilar().also { image ->
+                image.createGraphics().use { big ->
                     big.drawImage(Intro.Surface.bufferedImage, 0, 0, null)
-                    run {
-                        var x = 0
-                        while (x < w && scale > 0.0) {
-                            val ww = if (x + siw < w) siw else w - x
-                            run {
-                                var y = 0
-                                while (y < h) {
-                                    val hh = if (y + sih < h) sih else h - y
-                                    subs.add(bimg.getSubimage(x, y, ww, hh))
-                                    pts.add(Point(x, y))
-                                    y += sih
-                                }
-                            }
-                            x += siw
+                    for (x in 0 until w step subimageWidth) {
+                        if (scale <= 0.0) break
+                        val ww = if (x + subimageWidth < w) subimageWidth else w - x
+                        for (y in 0 until h step subimageHeight) {
+                            val hh = if (y + subimageHeight < h) subimageHeight else h - y
+                            subimages.add(image.getSubimage(x, y, ww, hh))
+                            points.add(Point(x, y))
                         }
                     }
                 }
             }
         }
-
-        rotate += rIncr
-        scale -= sIncr
+        rotate += rotateIncr
+        scale -= scaleIncr
     }
 
     override fun render(w: Int, h: Int, g2: Graphics2D) {
         val saveTx = g2.transform
         g2.color = Intro.myBlue
-        var i = 0
-        while (i < subs.size && scale > 0.0) {
-            val bi = subs[i]
-            val p = pts[i]
+        for (i in subimages.indices) {
+            if (scale <= 0.0) break
+            val bi = subimages[i]
+            val p = points[i]
             val ww = bi.width
             val hh = bi.height
-            val at = AffineTransform()
-            at.rotate(Math.toRadians(rotate), (p.x + ww / 2).toDouble(), (p.y + hh / 2).toDouble())
-            at.translate(p.x.toDouble(), p.y.toDouble())
-            at.scale(scale, scale)
-
+            val at = AffineTransform().apply {
+                rotate(Math.toRadians(rotate), (p.x + ww / 2).toDouble(), (p.y + hh / 2).toDouble())
+                translate(p.x.toDouble(), p.y.toDouble())
+                scale(scale, scale)
+            }
             val b1 = Rectangle(0, 0, ww, hh)
             val shape = at.createTransformedShape(b1)
             val b2 = shape.bounds2D
             val xx = p.x + ww / 2 - (b2.x + b2.width / 2)
             val yy = p.y + hh / 2 - (b2.y + b2.height / 2)
-            val toCenterAT = AffineTransform()
-            toCenterAT.translate(xx, yy)
-            toCenterAT.concatenate(at)
-
-            g2.transform = toCenterAT
+            g2.transform = AffineTransform().apply {
+                translate(xx, yy)
+                concatenate(at)
+            }
             g2.drawImage(bi, 0, 0, null)
             g2.draw(b1)
-            i++
         }
         g2.transform = saveTx
     }
