83604cbd427e9e0848e3ab70d1ce91b57251df99
Roman Dawydkin <tanmatra@gmail.com>
2018-08-27 11:47:59 +0300

    Refactor: TxE

--- src/main/java/java2d/intro/TxE.kt
+++ src/main/java/java2d/intro/TxE.kt
@@ -1,5 +1,6 @@
 package java2d.intro
 
+import java2d.hasBits
 import java.awt.AlphaComposite
 import java.awt.Composite
 import java.awt.Font
@@ -23,14 +24,14 @@ internal class TxE(
     override val end: Int
 ) : Part
 {
-    private var rIncr: Double = 0.toDouble()
-    private var sIncr: Double = 0.toDouble()
-    private var sx: Double = 0.toDouble()
-    private var sy: Double = 0.toDouble()
-    private var rotate: Double = 0.toDouble()
+    private var rIncr: Double = 0.0
+    private var sIncr: Double = 0.0
+    private var sx: Double = 0.0
+    private var sy: Double = 0.0
+    private var rotate: Double = 0.0
     private val shapes: Array<Shape>
     private val txShapes: Array<Shape?>
-    private val sw: Int
+    private val textWidth: Int
     private var numRev: Int = 0
 
     init {
@@ -38,22 +39,20 @@ internal class TxE(
         val chars = text.toCharArray()
         txShapes = arrayOfNulls(chars.size)
         val frc = FontRenderContext(null, true, true)
-        val tl = TextLayout(text, font, frc)
-        sw = tl.getOutline(null).bounds.getWidth().toInt()
-        shapes = Array(chars.size) { j ->
-            val s = chars[j].toString()
-            TextLayout(s, font, frc).getOutline(null)
-        }
+        val textLayout = TextLayout(text, font, frc)
+        textWidth = textLayout.getOutline(null).bounds.getWidth().toInt()
+        shapes = chars.map { c -> TextLayout(c.toString(), font, frc).getOutline(null) }
+            .toTypedArray()
     }
 
     private fun setIncrements(numRevolutions: Double) {
         this.numRev = numRevolutions.toInt()
         rIncr = 360.0 / ((end - begin) / numRevolutions)
         sIncr = 1.0 / (end - begin)
-        if (type and SCX != 0 || type and SCY != 0) {
+        if (type hasBits SCX || type hasBits SCY) {
             sIncr *= 2.0
         }
-        if (type and DEC != 0) {
+        if (type hasBits DEC) {
             rIncr = -rIncr
             sIncr = -sIncr
         }
@@ -70,7 +69,7 @@ internal class TxE(
                 sy = -1.0
             }
             else -> {
-                sy = if (type and DEC != 0) 1.0 else 0.0
+                sy = if (type hasBits DEC) 1.0 else 0.0
                 sx = sy
             }
         }
@@ -78,20 +77,20 @@ internal class TxE(
     }
 
     override fun step(w: Int, h: Int) {
-        var charWidth = (w / 2 - sw / 2).toFloat()
+        var charX = (w / 2 - textWidth / 2).toFloat()
 
         for (i in shapes.indices) {
             val at = AffineTransform()
             val maxBounds = shapes[i].bounds
-            at.translate(charWidth.toDouble(), h / 2 + maxBounds.getHeight() / 2)
-            charWidth += maxBounds.getWidth().toFloat() + 1
+            at.translate(charX.toDouble(), h / 2 + maxBounds.getHeight() / 2)
+            charX += maxBounds.getWidth().toFloat() + 1
             var shape = at.createTransformedShape(shapes[i])
             val b1 = shape.bounds2D
 
-            if (type and R != 0) {
+            if (type hasBits R) {
                 at.rotate(Math.toRadians(rotate))
             }
-            if (type and SC != 0) {
+            if (type hasBits SC) {
                 at.scale(sx, sy)
             }
             shape = at.createTransformedShape(shapes[i])
@@ -99,17 +98,18 @@ internal class TxE(
 
             val xx = b1.x + b1.width / 2 - (b2.x + b2.width / 2)
             val yy = b1.y + b1.height / 2 - (b2.y + b2.height / 2)
-            val toCenterAT = AffineTransform()
-            toCenterAT.translate(xx, yy)
-            toCenterAT.concatenate(at)
+            val toCenterAT = AffineTransform().apply {
+                translate(xx, yy)
+                concatenate(at)
+            }
             txShapes[i] = toCenterAT.createTransformedShape(shapes[i])
         }
         // avoid over rotation
         if (Math.abs(rotate) <= numRev * 360) {
             rotate += rIncr
             when {
-                type and SCX != 0 -> sx += sIncr
-                type and SCY != 0 -> sy += sIncr
+                type hasBits SCX -> sx += sIncr
+                type hasBits SCY -> sy += sIncr
                 else -> {
                     sx += sIncr
                     sy += sIncr
@@ -119,52 +119,51 @@ internal class TxE(
     }
 
     override fun render(w: Int, h: Int, g2: Graphics2D) {
-        var saveAC: Composite? = null
-        if (type and AC != 0 && sx > 0 && sx < 1) {
-            saveAC = g2.composite
-            g2.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, sx.toFloat())
+        val savedComposite: Composite? = if (type hasBits AC && sx > 0.0 && sx < 1.0) {
+            g2.composite.also {
+                g2.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, sx.toFloat())
+            }
+        } else {
+            null
         }
-        var path: GeneralPath? = null
-        if (type and CLIP != 0) {
-            path = GeneralPath()
-        }
+        val path: GeneralPath? = if (type hasBits CLIP) GeneralPath() else null
         if (paint != null) {
             g2.paint = paint
         }
-        for (i in txShapes.indices) {
-            if (type and CLIP != 0) {
-                path!!.append(txShapes[i], false)
+        for (txShape in txShapes) {
+            if (path != null) {
+                path.append(txShape, false)
             } else {
-                g2.fill(txShapes[i])
+                g2.fill(txShape)
             }
         }
-        if (type and CLIP != 0) {
+        if (path != null) {
             g2.clip(path)
         }
-        if (saveAC != null) {
-            g2.composite = saveAC
+        if (savedComposite != null) {
+            g2.composite = savedComposite
         }
     }
 
-    @Suppress("unused", "MemberVisibilityCanBePrivate")
+    @Suppress("unused")
     companion object
     {
-        const val INC = 1
-        const val DEC = 2
-        const val R = 4            // rotate
+        const val INC  =   1
+        const val DEC  =   2
+        const val R    =   4 // rotate
+        const val SC   =   8 // scale
+        const val SCX  =  16 // scale invert x
+        const val SCY  =  32 // scale invert y
+        const val AC   =  64 // AlphaComposite
+        const val CLIP = 128 // Clipping
+        const val NOP  = 512 // No Paint
         const val RI = R or INC
         const val RD = R or DEC
-        const val SC = 8            // scale
         const val SCI = SC or INC
         const val SCD = SC or DEC
-        const val SCX = 16           // scale invert x
         const val SCXI = SCX or SC or INC
         const val SCXD = SCX or SC or DEC
-        const val SCY = 32           // scale invert y
         const val SCYI = SCY or SC or INC
         const val SCYD = SCY or SC or DEC
-        const val AC = 64           // AlphaComposite
-        const val CLIP = 128          // Clipping
-        const val NOP = 512          // No Paint
     }
 }
